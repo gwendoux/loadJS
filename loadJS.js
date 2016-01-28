0fb3d00bb4979b2ba52612bde42302bdf3b72ebc
gwen <gwendoux@gmail.com>
2016-01-28 09:53:40 +0100

    Added ordered option
    forked until PR merge into main project
    https://github.com/filamentgroup/loadJS/pull/22

--- loadJS.js
+++ loadJS.js
@@ -1,11 +1,14 @@
 /*! loadJS: load a JS file asynchronously. [c]2014 @scottjehl, Filament Group, Inc. (Based on http://goo.gl/REQGQ by Paul Irish). Licensed MIT */
+// forked until PR merge into main project
+// https://github.com/filamentgroup/loadJS/pull/22
 (function( w ){
-	var loadJS = function( src, cb ){
+	var loadJS = function( src, cb, ordered ){
 		"use strict";
 		var ref = w.document.getElementsByTagName( "script" )[ 0 ];
 		var script = w.document.createElement( "script" );
 		script.src = src;
-		script.async = true;
+		// negation to keep consistency
+		script.async = !ordered;
 		ref.parentNode.insertBefore( script, ref );
 		if (cb && typeof(cb) === "function") {
 			script.onload = cb;
